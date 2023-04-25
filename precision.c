2a2aa7a569fe93023a2dc432265b9e51ea76d895
Imadeddinn <ouali.aimad.eddine@gmail.com>
2023-04-25 14:23:17 -0700

    Updated precision.c file

--- precision.c
+++ precision.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "print_functions.h"
+#include "print_functions.c"
 /**
 * get_precision - Calculates the precision for printing
 * @format: Formatted string in which to print the arguments
