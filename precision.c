ef5b80f55019e57c1658de6bcbc2dcaccba38053
Imadeddinn <ouali.aimad.eddine@gmail.com>
2023-04-25 13:03:07 -0700

    Updat precision.c file

--- precision.c
+++ precision.c
@@ -11,29 +11,3 @@
 *
 * Return: Precision.
 */
-
-int get_precision(const char *format, int *i, va_list list)
-{
-	int curr_i = *i + 1;
-	int precision = -1;
-
-	if (format[curr_i] == '.') {
-		precision = 0;
-		curr_i++;
-
-		while (is_digit(format[curr_i])) {
-			precision = precision * 10 + (format[curr_i] - '0');
-			curr_i++;
-		}
-
-		if (format[curr_i] == '*') {
-			curr_i++;
-			precision = va_arg(list, int);
-		}
-	}
-
-	*i = curr_i - 1;
-
-	return precision;
-}
-
