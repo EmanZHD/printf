86c9b8203ce04774dd0bbc680c1b7423a408093e
Imadeddinn <ouali.aimad.eddine@gmail.com>
2023-04-25 12:54:11 -0700

    Update precision.c file

--- precision.c
+++ precision.c
@@ -1,3 +1,6 @@
+#include <stdarg.h>
+#include <ctype.h>
+
 #include "main.h"
 /**
 * get_precision - Calculates the precision for printing
@@ -11,26 +14,26 @@
 
 int get_precision(const char *format, int *i, va_list list)
 {
-int curr_i = *i + 1;
-int precision = -1;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-if (format[curr_i] == '.') {
-precision = 0;
-curr_i++;
+	if (format[curr_i] == '.') {
+		precision = 0;
+		curr_i++;
 
-while (is_digit(format[curr_i])) {
-precision = precision * 10 + (format[curr_i] - '0');
-curr_i++;
-}
+		while (is_digit(format[curr_i])) {
+			precision = precision * 10 + (format[curr_i] - '0');
+			curr_i++;
+		}
 
-if (format[curr_i] == '*') {
-curr_i++;
-precision = va_arg(list, int);
-}
-}
+		if (format[curr_i] == '*') {
+			curr_i++;
+			precision = va_arg(list, int);
+		}
+	}
 
-*i = curr_i - 1;
+	*i = curr_i - 1;
 
-return precision;
+	return precision;
 }
 
