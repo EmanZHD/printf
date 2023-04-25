8cbb04a2cd7225b104fec8c7c2ce458ad8620ca8
Imadeddinn <ouali.aimad.eddine@gmail.com>
2023-04-25 15:24:18 -0700

    Update precision.c file

--- get_precision.c
+++ get_precision.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * precision - Calculates the precision for printing
+ * get_precision - Calculates the precision for printing
  * by Aimad and Imane
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
