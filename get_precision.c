b7664d27a44edbbd40a950480233f311e1a23f7d
Imadeddinn <ouali.aimad.eddine@gmail.com>
2023-04-26 07:49:19 -0700

    Update precision.c file

--- get_precision.c
+++ get_precision.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * get_precision - Calculates the precision for printing
- * by Aimad and Imane
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
- */
+* get_precision - Calculates the precision for printing
+* by Aimad and Imane
+* @format: Formatted string in which to print the arguments
+* @i: List of arguments to be printed.
+* @list: list of arguments.
+*
+* Return: Precision.
+*/
 int get_precision(const char *format, int *i, va_list list)
 {
 int curr_i = *i + 1;
@@ -32,8 +32,12 @@ while (is_digit(format[curr_i]))
 {
 precision = precision * 10 + (format[curr_i] - '0');
 curr_i++;
+
 }
 }
+
+if (precision == 0)
+precision = -1;
 
 *i = curr_i - 1;
 
