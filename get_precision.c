75f56bcb17ebb3393d90e6575f48c9b79d8bed4d
Imadeddinn <ouali.aimad.eddine@gmail.com>
2023-04-26 07:30:10 -0700

    Updated precision.c file

--- get_precision.c
+++ get_precision.c
@@ -1,41 +1,38 @@
 #include "main.h"
+
 /**
- * get_precision - Calculates the precision for printing
- * by Aimad and Imane
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
- */
+* get_precision - Calculates the precision for printing
+* @format: Formatted string in which to print the arguments
+* @i: List of arguments to be printed.
+* @list: list of arguments.
+*
+* Return: Precision.
+*/
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+int curr_i = *i + 1;
+int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
+if (format[curr_i] != '.')
+return (precision);
 
-	precision = 0;
+precision = 0;
+curr_i++;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (precision);
+if (format[curr_i] == '*') {
+curr_i++;
+precision = va_arg(list, int);
+if (precision < 0)
+precision = -1; // treat negative value as no precision
+} else {
+while (is_digit(format[curr_i])) {
+precision = precision * 10 + (format[curr_i] - '0');
+curr_i++;
 }
+}
+
+*i = curr_i - 1;
+
+return (precision);
+}
+
